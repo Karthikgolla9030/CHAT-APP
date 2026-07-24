95c0ec15c3a7e8871456fbddebba524ffa67b316
Karthikgolla9030 <karthikgolla493@gmail.com>
2026-07-24 19:40:34 +0530

    Fix ASGI initialization order for Django settings

--- omniroute/asgi.py
+++ omniroute/asgi.py
@@ -3,15 +3,18 @@ ASGI config for OmniRoute project.
 """
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omniroute.settings')
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 import notifications.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omniroute.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns + notifications.routing.websocket_urlpatterns)
     ),
